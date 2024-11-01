178cd2b2b98824376ff2339314fa34309209cdd6
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2024-11-01 09:20:35 -0400

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -1,12 +1,19 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 public class Divisors
 {
 	public static String getDivisors( int number )
-	{
-		String divisors="";
+   
+	{   String divisors= "";
+        int placeholder = 1;
+        do 
+        {
+            if (number % placeholder ==0)
+            {
+                divisors += placeholder + " ";
+            }
+            placeholder = (placeholder +1);
+        }
+        while (placeholder <= number);
+		
 		return divisors; 
 	}
 }
